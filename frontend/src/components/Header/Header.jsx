b594141395913e705cf7f1bd15c3e87d2f9ff760
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2024-08-25 07:39:19 +0800

    added line animation in the links

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
-function Header() {
-    return(
-        <div className="header">
-            <div className="my-name">Joshnee</div>
-            <div className="right-buttons">
-                <a href="/">Home</a>
-                <a href="/About">About</a>
-                <a href="/Projects">Projects</a>
-                <a href="/Skills">Skills</a>
-                <a href="/Contact">Contact</a>
-            </div>
+const Header = () => {
+    const [activeLink, setActiveLink] = useState("/");
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
+    return (
+        <header className="header">
+        <div className="brand">
+            <h1>Joshnee</h1>
         </div>
+        <nav className="navigation">
+            <ul className="nav-list">
+            <li>
+                <Link
+                to="/"
+                className={`nav-link ${activeLink === "/" ? "underline" : ""}`}
+                onClick={() => handleLinkClick("/")}
+                >
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link
+                to="/skills"
+                className={`nav-link ${
+                    activeLink === "/skills" ? "underline" : ""
+                }`}
+                onClick={() => handleLinkClick("/skills")}
+                >
+                    Skills
+                </Link>
+            </li>
+            <li>
+                <Link
+                to="/projects"
+                className={`nav-link ${
+                    activeLink === "/projects" ? "underline" : ""
+                }`}
+                onClick={() => handleLinkClick("/projects")}
+                >
+                    Projects
+                </Link>
+            </li>
+            <li>
+                <Link
+                to="/about"
+                className={`nav-link ${
+                    activeLink === "/about" ? "underline" : ""
+                }`}
+                onClick={() => handleLinkClick("/about")}
+                >
+                    About
+                </Link>
+            </li>
+            <li>
+                <Link
+                to="/contacts"
+                className={`nav-link ${
+                    activeLink === "/contacts" ? "underline" : ""
+                }`}
+                onClick={() => handleLinkClick("/contacts")}
+                >
+                    Contacts
+                </Link>
+            </li>
+            </ul>
+        </nav>
+        </header>
     );
-}
+};
 
 export default Header;
